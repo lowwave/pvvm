e927c7a0af87374e839fe93eee3384e86e87a9dd
Andrii Antosha <indieamindie@gmail.com>
2021-04-05 20:45:34 +0200

    implement stack

--- cpu.js
+++ cpu.js
@@ -8,7 +8,8 @@ class CPU {
     this.registerNames = [
       'ip', 'acc',
       'r1', 'r2', 'r3', 'r4',
-      'r5', 'r6', 'r7', 'r8'
+      'r5', 'r6', 'r7', 'r8',
+      'sp', 'fp'
     ]
 
     this.registers = createMemory(this.registerNames.length * 2)
@@ -17,6 +18,11 @@ class CPU {
       map[name] = i * 2
       return map
     }, {})
+
+    this.setRegister('sp', memory.byteLength - 1 - 1)
+    this.setRegister('fp', memory.byteLength - 1 - 1)
+
+    this.stackFrameSize = 0;
   }
 
   debug() {
@@ -26,12 +32,12 @@ class CPU {
     console.log()
   }
 
-  viewMemoryAt(address) {
-    const nextEightBytes = Array.from({ length: 8 }, (_, i) =>
+  viewMemoryAt(address, n = 8) {
+    const nextNBytes = Array.from({ length: n }, (_, i) =>
       this.memory.getUint8(address + i)
     ).map(v => `0x${v.toString(16).padStart(2, '0')}`)
 
-    console.log(`0x${address.toString(16).padStart(4, '0')}: ${nextEightBytes.join(' ')}`)
+    console.log(`0x${address.toString(16).padStart(4, '0')}: ${nextNBytes.join(' ')}`)
   }
 
   getRegister(name) {
@@ -65,6 +71,65 @@ class CPU {
     
     return instruction
   }
+  
+  fetchRegisterIndex() {
+    return (this.fetch() % this.registerNames.length) * 2
+  }
+
+  push(value) {
+    const spAddress = this.getRegister('sp')
+    this.memory.setUint16(spAddress, value)
+    this.setRegister('sp', spAddress - 2)
+    this.stackFrameSize += 2
+  }
+
+  pop() {
+    const nextSpAddress = this.getRegister('sp') + 2
+    this.setRegister('sp', nextSpAddress)
+    this.stackFrameSize -= 2
+    return this.memory.getUint16(nextSpAddress)
+  }
+
+  pushState() {
+    this.push(this.getRegister('r1'))
+    this.push(this.getRegister('r2'))
+    this.push(this.getRegister('r3'))
+    this.push(this.getRegister('r4'))
+    this.push(this.getRegister('r5'))
+    this.push(this.getRegister('r6'))
+    this.push(this.getRegister('r7'))
+    this.push(this.getRegister('r8'))
+    this.push(this.getRegister('ip'))
+    this.push(this.stackFrameSize + 2)
+
+    this.setRegister('fp', this.getRegister('sp'))
+    this.stackFrameSize = 0
+  }
+
+  popState() {
+    const framePointerAddress = this.getRegister('fp')
+    this.setRegister('sp', framePointerAddress)
+
+    this.stackFrameSize = this.pop()
+    const stackFrameSize = this.stackFrameSize
+
+    this.setRegister('ip', this.pop())
+    this.setRegister('r8', this.pop())
+    this.setRegister('r7', this.pop())
+    this.setRegister('r6', this.pop())
+    this.setRegister('r5', this.pop())
+    this.setRegister('r4', this.pop())
+    this.setRegister('r3', this.pop())
+    this.setRegister('r2', this.pop())
+    this.setRegister('r1', this.pop())
+
+    const nArgs = this.pop()
+    for (let i = 0; i < nArgs; i++) {
+      this.pop()
+    }
+
+    this.setRegister('fp', framePointerAddress + stackFrameSize)
+  }
 
   step() {
     const instruction = this.fetch()
@@ -76,15 +141,15 @@ class CPU {
       // move literal value into the register
       case instructions.MOV_LIT_REG: {
         const literal = this.fetch16()
-        const register = (this.fetch() % this.registerNames.length) * 2
+        const register = this.fetchRegisterIndex()
         this.registers.setUint16(register, literal)
         return
       }
 
       // move register to register
       case instructions.MOV_REG_REG: {
-        const registerFrom = (this.fetch() % this.registerNames.length) * 2
-        const registerTo = (this.fetch() % this.registerNames.length) * 2
+        const registerFrom = this.fetchRegisterIndex()
+        const registerTo = this.fetchRegisterIndex()
         const value = this.registers.getUint16(registerFrom)
         this.registers.setUint16(registerTo, value)
         return
@@ -92,7 +157,7 @@ class CPU {
 
       // move register to memory
       case instructions.MOV_REG_MEM: {
-        const registerFrom = (this.fetch() % this.registerNames.length) * 2
+        const registerFrom = this.fetchRegisterIndex()
         const address = this.fetch16()
         const value = this.registers.getUint16(registerFrom)
         this.memory.setUint16(address, value)
@@ -102,7 +167,7 @@ class CPU {
       // move memory to register
       case instructions.MOV_MEM_REG: {
         const address = this.fetch16()
-        const registerTo = (this.fetch() % this.registerNames.length) * 2
+        const registerTo = this.fetchRegisterIndex()
         const value = this.memory.getUint16(address)
         this.registers.setUint16(registerTo, value)
         return
@@ -110,8 +175,8 @@ class CPU {
 
       // add register to register
       case instructions.ADD_REG_REG: {
-        const r1 = this.fetch()
-        const r2 = this.fetch()
+        const r1 = this.fetchRegisterIndex()
+        const r2 = this.fetchRegisterIndex()
         const registerValue1 = this.registers.getUint16(r1 * 2)
         const registerValue2 = this.registers.getUint16(r2 * 2)
         this.setRegister('acc', registerValue1 + registerValue2)
@@ -128,7 +193,46 @@ class CPU {
 
         return
       }
-    
+      
+      // push literal
+      case instructions.PSH_LIT: {
+        const value = this.fetch16()
+        this.push(value)
+        return
+      }
+
+      // push register
+      case instructions.PSH_REG: {
+        const registerIndex = this.fetchRegisterIndex()
+        this.push(this.registers.getUint16(registerIndex))
+        return
+      }
+
+      case instructions.POP: {
+        const registerIndex = this.fetchRegisterIndex()
+        const value = this.pop()
+        this.registers.setUint16(registerIndex, value)
+        return
+      }
+
+      case instructions.CAL_LIT: {
+        const address = this.fetch16()
+        this.pushState()
+        this.setRegister('ip', address)
+      }
+
+      case instructions.CAL_REG: {
+        const registerIndex = this.fetchRegisterIndex()
+        const address = this.registers.getUint16(registerIndex)
+        this.pushState()
+        this.setRegister('ip', address)
+        return
+      }
+
+      case instructions.RET: {
+        this.popState()
+        return
+      }
       
       default:
         break;
--- index.js
+++ index.js
@@ -7,42 +7,89 @@ const IP = 0
 const ACC = 1
 const R1 = 2
 const R2 = 3
+const R3 = 4
+const R4 = 5
+const R5 = 7
+const R6 = 8
+const R7 = 9
+const R8 = 10
+const SP = 11
+const FP = 12
 
 const memory = createMemory(256 * 256)
 const writableBytes = new Uint8Array(memory.buffer)
 
 const cpu = new CPU(memory)
 
+const subroutineAddress = 0x3000
+
 let i = 0
 
-writableBytes[i++] = instructions.MOV_MEM_REG
-writableBytes[i++] = 0x01
-writableBytes[i++] = 0x00 // 0x0100
+writableBytes[i++] = instructions.PSH_LIT
+writableBytes[i++] = 0x33
+writableBytes[i++] = 0x33
+
+
+writableBytes[i++] = instructions.PSH_LIT
+writableBytes[i++] = 0x22
+writableBytes[i++] = 0x22
+
+writableBytes[i++] = instructions.PSH_LIT
+writableBytes[i++] = 0x11
+writableBytes[i++] = 0x11
+
+writableBytes[i++] = instructions.MOV_LIT_REG
+writableBytes[i++] = 0x12
+writableBytes[i++] = 0x34
 writableBytes[i++] = R1
 
 writableBytes[i++] = instructions.MOV_LIT_REG
+writableBytes[i++] = 0x56
+writableBytes[i++] = 0x78
+writableBytes[i++] = R4
+
+writableBytes[i++] = instructions.PSH_LIT
 writableBytes[i++] = 0x00
-writableBytes[i++] = 0x01 // 0x0001
-writableBytes[i++] = R2
+writableBytes[i++] = 0x00
 
-writableBytes[i++] = instructions.ADD_REG_REG
-writableBytes[i++] = R1
-writableBytes[i++] = R2
+writableBytes[i++] = instructions.CAL_LIT
+writableBytes[i++] = (subroutineAddress & 0xff00) >> 8
+writableBytes[i++] = (subroutineAddress & 0x00ff)
 
-writableBytes[i++] = instructions.MOV_REG_MEM
-writableBytes[i++] = ACC
+writableBytes[i++] = instructions.PSH_LIT
+writableBytes[i++] = 0x44
+writableBytes[i++] = 0x44
+
+// subroutine
+i = subroutineAddress
+
+writableBytes[i++] = instructions.PSH_LIT
 writableBytes[i++] = 0x01
-writableBytes[i++] = 0x00 // 0x0100
+writableBytes[i++] = 0x02
 
-writableBytes[i++] = instructions.JMP_NOT_EQ
-writableBytes[i++] = 0x00
-writableBytes[i++] = 0x03 // 0x0003
-writableBytes[i++] = 0x00
-writableBytes[i++] = 0x00 // 0x0000
+writableBytes[i++] = instructions.PSH_LIT
+writableBytes[i++] = 0x03
+writableBytes[i++] = 0x04
+
+writableBytes[i++] = instructions.PSH_LIT
+writableBytes[i++] = 0x05
+writableBytes[i++] = 0x06
+
+writableBytes[i++] = instructions.MOV_LIT_REG
+writableBytes[i++] = 0x07
+writableBytes[i++] = 0x08
+writableBytes[i++] = R1
+
+writableBytes[i++] = instructions.MOV_LIT_REG
+writableBytes[i++] = 0x09
+writableBytes[i++] = 0x0A
+writableBytes[i++] = R8
+
+writableBytes[i++] = instructions.RET
 
 cpu.debug()
 cpu.viewMemoryAt(cpu.getRegister('ip'))
-cpu.viewMemoryAt(0x0100)
+cpu.viewMemoryAt(0xffff - 1 - 42, 44)
   
 const rl = readline.createInterface({
   input: process.stdin,
@@ -53,5 +100,5 @@ rl.on('line', () => {
   cpu.step()
   cpu.debug()
   cpu.viewMemoryAt(cpu.getRegister('ip'))
-  cpu.viewMemoryAt(0x0100)
+  cpu.viewMemoryAt(0xffff - 1 - 42, 44)
 })
